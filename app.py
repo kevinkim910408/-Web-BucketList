f9292ed5d9285282d9c60c6e3683abb7cca5958f
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-04-21 11:00:22 -0400

    Done

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__) # Flask 객체 생성
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.m7jzf.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/') # @app.route('/') 는 http://localhost:5000/ 를 가리킵니다.
+def home():
+    return render_template('index.html')
+
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form["bucket_give"]
+    count = db.bucket.count_documents({'_id': False})
+    num = count + 1
+    doc = {
+        'num': num,
+        'bucket': bucket_receive,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'Saved!'})
+
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': 'Finished!'})
+
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
